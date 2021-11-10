a221a7a3388ef3f39ac18501895343d634cee62b
Alex Mehler <alexmehler@t-online.de>
2021-11-10 15:46:20 +0100

    Add files via upload

--- render_eevee_viewport_animation_without_ocio_to_ffmpeg.py
+++ render_eevee_viewport_animation_without_ocio_to_ffmpeg.py
@@ -13,22 +13,13 @@ startTime = time.time() # measure rendering time
 
 # get original Settings
 origRenderPath = bpy.context.scene.render.filepath
-origLook = bpy.context.scene.view_settings.look
+origLook = bpy.context.scene.view_settings.look # im using an ocio file to preview the real colors on my widegamut monitor , ocio is chosen in render -> color management -> look
 origFileFormat = bpy.context.scene.render.image_settings.file_format
-
-
-
-finalRenderPath = bpy.context.scene.render.filepath + ".mov"
-
-startFrame = bpy.context.scene.frame_start
-fps = bpy.context.scene.render.fps
-
-
 
 # -------------------------- set render settings for temp files ---------------------------------------
 
 # disable ocio preview / render with normal rgb values , a lut can be easily added in post instead, 
-# eevee is not good at applying lut(ocio), banding and wrong colors are the result but for preview its better than nothing
+# eevee is not good at applying lut(ocio), banding and slighly wrong colors are the result but for preview its better than nothing
 bpy.context.scene.view_settings.look = 'None'
 
 # set tempRenderPath
@@ -40,14 +31,19 @@ except FileExistsError:
 
 bpy.context.scene.render.filepath = os.path.join(tempRenderPath, 'temp') # i.e. f:\temp\Blender_Render_Temp\temp####.png
 
-# set to png or openEXR  !!! important set up the values beforehand i.e. png with rgba 16bit or exr with halffloat and alpha
+# set to png  or whatever you like!!! important set up the values for the codec beforehand i.e. png with rgba 16bit or add them to the script for full automation
 bpy.context.scene.render.image_settings.file_format = "PNG"
 
 
 # ------------------------- RENDER -----------------------------------------------
-#bpy.ops.render.opengl(animation=True) # opengl = viewport rendering ! my preferred method , for my toon stuff i dont need more than that
+bpy.ops.render.opengl(animation=True) # opengl = viewport rendering ! my preferred method , for my toon stuff i dont need more than that
 
 # ------------------------- CONVERT with ffmpeg to your favourite format (prores, cineform, nvenc ???) -------------------
+
+# get render settings fps and framestart , we need them for ffmpeg
+startFrame = bpy.context.scene.frame_start
+fps = bpy.context.scene.render.fps
+finalRenderPath = bpy.context.scene.render.filepath + ".mov" # filename for ffmpeg 
 
 # !! this is for windows ! in inux the syntax is slightly different !
 # cineform codec with preset filmscan1 (filmscan2 is about the same data rate as prores 4444)  rgba 12bit  ; -y means overwright file
@@ -56,9 +52,8 @@ cmdParameters=f'ffmpeg -framerate {fps} -start_number {startFrame} -i {tempRende
 #cmdParameters=f'ffmpeg -framerate {fps} -start_number {startFrame} -i {tempRenderPath}\\temp%04d.png -c:v prores_ks -pix_fmt yuva444p10le -alpha_bits 16 -profile:v 4444 -map v:0 -y "{finalRenderPath}"'
 # in theory it should be possible to call any codec or other tools like adobe media encoder from here.
 
-print(cmdParameters)
+#print(cmdParameters)
 os.system(cmdParameters)
-#subprocess.run(['cmd',cmdParameters])
 
 # ------------------------- delete files in the temp folder ----------------------
 
